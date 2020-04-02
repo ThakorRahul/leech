b5d54d4e31bfaedfafafde15ea951b69df2e23ed
ThakorRahul <56543994+ThakorRahul@users.noreply.github.com>
2020-04-02 22:00:57 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -33,12 +33,12 @@ async def new_join_f(client, message):
 
 
 async def help_message_f(client, message):
-    # await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
+    # await message.reply_text("no one gonna help you ðŸ¤£ðŸ¤£ðŸ¤£ðŸ¤£", quote=True)
     channel_id = str(AUTH_CHANNEL)[4:]
     message_id = 99
     # display the /help message
     await message.reply_text(
-        f"please read the <a href='https://t.me/c/{channel_id}/{message_id}'>Pinned Message</a>",
+        f"Please Read the <a href='https://t.me/c/{channel_id}/{message_id}'>Pinned Message</a>",
         quote=True
     )
 
